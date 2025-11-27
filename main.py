9a9cdc8cbaf1885af2b32e011dbddd6a0a1c6196
Arajen <arajenkajanthira@gmail.com>
2025-11-26 20:47:41 -0500

    Main updated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Adventure Game 
+
 import random
 # Character Class
 class Character:
@@ -39,12 +40,18 @@ class Character:
         print(f"{target.name}'s health is now {target.health} HP")
         if not target.is_alive():
             print(f"{target.name} has been defeated!")
+    def show_stats(self):
+        print(f"\n{self.name} | Level {self.level}")
+        print(f"HP: {self.health}/{self.max_health}")
+        print(f"STR: {self.strength} | DEF: {self.defense} | CRIT: {self.crit}%")
     
 # Player Class
 class Player(Character):
     def __init__(self, name, health, strength, defense, crit):
         super().__init__(name, health, strength, defense, crit)
-
+        self.level = 1
+        self.xp = 0
+        self.xp_to_level = 100 #xp needed to lvl up
     def heal(self):
         heal_amount = 15
         self.health += heal_amount
@@ -52,6 +59,15 @@ class Player(Character):
             self.health = self.max_health
         print(f'{self.name} healed for {heal_amount} HP!')
         print(f"{self.name}'s health is now {self.health}/{self.max_health} HP")
+    def level_up(self):
+        self.level +=1
+        self.max_health +=10
+        self.health = self.max_health #heals to full on lvl up
+        self.strength += 2
+        self.defense += 1
+        self.crit +=1
+        print(f"{self.name} leveled up to Level {self.level}!")
+        print(f"Stats increased: +10 HP, +2 STR, +1 DEF, +1% CRIT")
 # Enemy Class
 class Enemy(Character):
     def __init__(self, name, health, strength, defense, crit):
@@ -85,6 +101,11 @@ class Battle:
             round_number+=1  
         if self.player.is_alive():
             print(f"{self.player.name} wins the battle!")
+            self.player.xp +=25
+            print(f'{self.player.name} gained 25 XP')
+            if self.player.xp >= self.player.xp_to_level:
+                self.player.level_up()
+                self.player.xp -= self.player.xp_to_level
         else:
             print(f"{self.enemy.name} wins the battle...")     
 # Main 
